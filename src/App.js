db9af5f78365455f6a39bc2fc3e499b2e126ce1a
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-22 22:00:25 -1000

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div id="app" className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
