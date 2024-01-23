39a7a5060370f04d854aaaf1dd44645939795e0e
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-23 12:57:37 -1000

    About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
+import About from './components/About';
 
 const App = () => {
   return (
     <div id="app" className="App">
       <Header />
       <Body />
+      <About />
     </div>
   );
 }
