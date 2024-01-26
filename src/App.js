0919070281ddd296592e08ffbc188f1998f5446c
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-26 12:04:24 -1000

    project section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import About from './components/About';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Body />
       <About />
+      <Projects />
     </div>
   );
 }
