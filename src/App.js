5e1e2df10055b7352b15cf314beb225ce169734c
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-26 17:41:31 -1000

    footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Body from './components/Body';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Projects />
       <Skills />
+      <Footer />
     </div>
   );
 }
